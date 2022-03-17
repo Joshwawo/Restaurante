983f8860395965a8b233ac39ff2496ed015f4569
Joshwawo <L17330479@hermosillo.tecnm.mx>
2022-03-17 14:04:36 -0700

    Validacion con Js

--- src/Vista/pages/script.js
+++ src/Vista/pages/script.js
@@ -10,6 +10,8 @@ var contenedor_login_register = document.querySelector(".contenedor__login-regis
 var caja_trasera_login = document.querySelector(".caja__trasera-login");
 var caja_trasera_register = document.querySelector(".caja__trasera-register");
 
+
+
 //FUNCIONES
 
 function anchoPage() {
@@ -61,5 +63,82 @@ function register() {
         caja_trasera_login.style.display = "block";
         caja_trasera_login.style.opacity = "1";
     }
-}       
+}
+const datos = {
+    nombre: '',
+    correo: '',
+    usuario: '',
+    contrasena: '',
+    direct: ''
+
+}
+
+const nombreInput = document.querySelector('#nombre');
+const coreoInput = document.querySelector('#correo');
+const usuarioInput = document.querySelector('#usuario');
+const contrasenaInput = document.querySelector('#contrasena');
+const directInput = document.querySelector('#direct');
+const formulario = document.querySelector('.formulario__register');
+
+// nombreInput.addEventListener('input', function(a){
+//     console.log(object);
+// })
+
+
+
+nombreInput.addEventListener('input', leerTexto);
+coreoInput.addEventListener('input', leerTexto);
+usuarioInput.addEventListener('input', leerTexto);
+contrasenaInput.addEventListener('input', leerTexto);
+directInput.addEventListener('input', leerTexto);
+
+formulario.addEventListener('submit', function (e) {
+
+
+    const { nombre, correo, usuario, contrasena, direct } = datos;
+    if (nombre === '' || correo === '' || usuario === '' || contrasena === '' || direct == '') {
+        mostrarError('todos los campos son obligatorios');
+
+        return;
+    }
+
+    mostrarRegistro('Te has Registrado Correctamente!');
+
+
+});
+
+function mostrarError(mensaje) {
+    const error = document.createElement('P');
+    error.textContent = mensaje;
+    error.classList.add('error');
+    formulario.appendChild(error)
+    console.log(error);
+
+    setTimeout(() => {
+        error.remove();
+
+    }, 3000);
+
+}
+
+function mostrarRegistro(mensaje) {
+    const completo = document.createElement('P');
+    completo.textContent = mensaje;
+    completo.classList.add('completo');
+    formulario.appendChild(completo)
+    console.log(completo);
+
+    setTimeout(() => {
+        completo.remove();
+    }, 3000);
+}
+
+
+
+function leerTexto(e) {
+    datos[e.target.id] = e.target.value;
+    console.log(e.target.value);
+}
+
+
 
