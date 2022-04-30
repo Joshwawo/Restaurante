9d266bd58a2d4a3be244ddbb16b18d04b745c4a4
Joshwawo <L17330479@hermosillo.tecnm.mx>
2022-04-29 18:46:05 -0700

    ultimo commit

--- src/Vista/pages/admin/pages/homepageN.php
+++ src/Vista/pages/admin/pages/homepageN.php
@@ -174,8 +174,72 @@ if (isset($_POST['add_to_complete'])) {
         </section>
 
         
-       <section>
+       
+        <section class="shopping-cart">
+        <h2 style="color: white; font-size: 2rem; text-align: center;">En proceso</h2>
+       <table  class="tabla hp">
+
+                <thead>
+                    <th>Nombre</th>
+                    <th>Metodo Pago</th>
+                    <th>Direccion</th>
+                    <th>Colonia</th>
+                    <th>Comentario</th>
+                    <th>Productos</th>
+                    <th>Precio Total</th>
+                  
+                    
+
+
+                </thead>
+
+                <tbody>
+                    <?php
+
+                    /**
+                     * La variable $select_cart seleciona nuestra tabla llamada "Cart" de nuestra base de datos
+                     */
+                    $select_cart = mysqli_query($conn, "SELECT * FROM `order`");
+
+
+                    /**
+                     * Esta variable es el precio total a pagar por el cliente - se inicia en cero porque no hay nada en el carrito
+                     */
+                    $grand_total = 0;
+                    if (mysqli_num_rows($select_cart) > 0) {
+                        while ($fetch_cart = mysqli_fetch_assoc($select_cart)) {
+                    ?>
+
+                            <tr>
+                                <td class="td__admin"><?php echo $fetch_cart['name'] ?></td>
+                                <td class="td__admin"><?php echo $fetch_cart['method'] ?></td>
+                                <td class="td__admin"><?php echo $fetch_cart['flat'] ?></td>
+                                <td class="td__admin"><?php echo $fetch_cart['city'] ?></td>
+                                <td class="td__admin"><?php echo $fetch_cart['country'] ?></td>
+                                <td class="td__admin"><?php echo $fetch_cart['total_products'] ?></td>
+                                    
+                                <td class="td__admin"><?php echo $fetch_cart['total_price'] ?> Mxn.</td>
+                                <td><button class="btn">Completo</button></td>
+                                
+                                 <!-- <td class="td__admin"><a href="?remove=<?php echo $fetch_cart['id']; ?>" onclick="return confirm('¿Estas Seguro de eliminar esto del carrito?')" class="delete-btn">  Eliminar</a></td> -->
+
+                            </tr>
+
+                    <?php
+                            // $grand_total += $sub_total;
+                        };
+                    };
+                    ?>
+
+                </tbody>
+                <td><a href="../../admin/pages/homepage.php?delete_all" onclick="return confirm('¿Estas Seguro que quieres eliminar todos los Pedidos?');" class="delete-btn"> <i class="fas fa-trash"></i> Eliminar Todo </a></td>
+            </table>
+
+       </section>
         
+    
+       <section class="shopping-cart">
+       <h2 style="color: white; font-size: 2rem; text-align: center; padding-bottom:1 rem">Completas</h2>
        <table  class="tabla hp">
 
                 <thead>
@@ -233,7 +297,6 @@ if (isset($_POST['add_to_complete'])) {
             </table>
 
        </section>
-
     </div>
 
     <!-- <script src="../../.././pages/admin/pages/tabla.js"></script> -->
